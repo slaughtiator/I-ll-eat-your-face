600666a536068e1bbe428d5c5746ef2e25bcac62
slaughtiator <mattslaughter6@gmail.com>
2020-04-28 20:20:57 -0500

    first

---
+++ Our first adventure/our first adventure.js
@@ -0,0 +1,15 @@
+/*
+I will eat your face in half... slowly
+Me too.
+*/
+
+// https://www.w3schools.com/js/js_comments.asp
+
+function setup(){
+  createCanvas(1280,700);
+}
+
+function draw(){
+  background(204);
+  ellipse(width/2, height/2, 50,50);
+}
